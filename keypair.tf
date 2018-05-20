f6314032287dd4bfb20dd57f5248570aad5bb862
Anmol Nagpal <ianmolnagpal@gmail.com>
2018-05-21 02:15:24 +0400

    update

--- keypair.tf
+++ keypair.tf
@@ -7,5 +7,5 @@
 ## keypair for ec2
 resource "aws_key_pair" "ecs-key" {
   key_name   = "ecs-key-${var.environment}"
-  public_key = "${file("./_ssh/live-instance-key.pub")}"
+  public_key = "${file("./_ssh/instance-key.pub")}"
 }
