6b96936fcfff07fdf60aa06117f792e2ae277f4b
Anmol Nagpal <ianmolnagpal@gmail.com>
2018-05-21 02:02:02 +0400

    update

---
+++ keypair.tf
@@ -0,0 +1,11 @@
+###_  __  _____  __   __  ____       _      ___   ____
+##| |/ / | ____| \ \ / / |  _ \     / \    |_ _| |  _ \
+##| ' /  |  _|    \ V /  | |_) |   / _ \    | |  | |_) |
+##| . \  | |___    | |   |  __/   / ___ \   | |  |  _ <
+##|_|\_\ |_____|   |_|   |_|     /_/   \_\ |___| |_| \_\
+
+## keypair for ec2
+resource "aws_key_pair" "ecs-key" {
+  key_name   = "ecs-key-${var.environment}"
+  public_key = "${file("./../../_ssh/live-instance-key.pub")}"
+}
